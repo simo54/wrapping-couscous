125949ea42898318a13a1227c11fcd873688d9a3
simone <sorzesimone1@gmail.com>
2021-05-20 21:36:47 +0200

    test

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,10 +1,9 @@
-import React from "react";
-
 // Wrapper
+import React from "react";
 
 export default function Wrapper({ title, anotherTitle, children }) {
   return (
-    <div className={title}>
+    <div className="yepthatsme">
       <h2>{title}</h2>
       {children} {/** Our children will stay within the wrapper */}
       <h2>{anotherTitle}</h2>
